0ff2dbadb167a396426a127f53178dbae4644ec6
Ben Bowers <nanobowers@gmail.com>
2021-06-12 16:17:57 -0400

    adding git aliases

---
+++ home/git_aliases.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git config --global alias.co 'checkout'
+git config --global alias.aa 'add -A'
+git config --global alias.master 'checkout main'
+git config --global alias.master 'checkout master'
+git config --global alias.aliases 'config --global -l'
+
