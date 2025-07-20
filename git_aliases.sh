502f9be5da79a2fb666dcb448881584771e63139
Ben Bowers <nanobowers@gmail.com>
2025-07-20 15:20:45 -0400

    fixing git aliases

--- git_aliases.sh
+++ git_aliases.sh
@@ -2,7 +2,7 @@
 
 git config --global alias.co 'checkout'
 git config --global alias.aa 'add -A'
-git config --global alias.master 'checkout main'
+git config --global alias.main 'checkout main'
 git config --global alias.master 'checkout master'
 git config --global alias.aliases 'config --global -l'
-
+git config --global alias.publish '!f() { git push --set-upstream ${1:-origin} $(git branch show-current); }; f'
